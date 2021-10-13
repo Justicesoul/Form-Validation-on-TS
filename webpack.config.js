b9c4bf87a430ad1463cd2145a611b8d9af6751ea
Serzsh <serzants.janis@gmail.com>
2021-10-13 22:11:00 +0300

    Added scss support

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,46 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: ['./src/scripts.ts', './src/styles.scss'],
   mode: 'development',
   devServer: {
     port: 3000,
     historyApiFallback: true,
+    client: {
+      logging: 'none',
+    },
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: '[name].css'}
+          },
+          'sass-loader'
+        ],
         exclude: /node_modules/,
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    favicon: 'favicon.png',
+    inject: false,
+  })],
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: 'scripts.js',
   },
 };
